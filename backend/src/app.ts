80fcd7948c14671e1fd936f60bce6028872e4a62
Kelly Kang <flrhyme9@gmail.com>
2023-12-27 16:04:26 +0900

    add dependencies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({ credentials: true, origin: 'http://localhost:3000'}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/welcome', (req: Request, res: Response) => {
   res.send("welcome");
 })
 app.get('/hello', (req: Request, res: Response) => {
-  res.send("hello");
+  res.json({msg: 'hello from server'});
 })
 
 app.listen('3099', () => {
