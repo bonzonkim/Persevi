50797b6bf9e5779505982255ce68de0822b3dbd3
Kelly Kang <flrhyme9@gmail.com>
2024-01-03 20:56:25 +0900

    add: 로고이미지, 푸터 아이콘

--- frontend/components/layout/footer.tsx
+++ frontend/components/layout/footer.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import {FaInstagram} from 'react-icons/fa';
+import { MdOutlineMailOutline } from "react-icons/md";
+import Link from "next/link";
 
 
 export default function Footer() {
     return (
         <>
-            <div className="flex flex-col">
-                <div className="container mx-auto p-4">
+            <div className="bg-persevi-grey py-4 w-screen footer bottom-0">
+                <div className='mx-3'>
+                    <footer className='flex flex-row text-white text-center items-center space-x-5 > *'>
+                        <Link href='https://instagram.com' target='_blank'>
+                            <FaInstagram/>
+                        </Link>
+                        <Link href='mailto:flrhyme9@gmail.com' target='_blank'>
+                            <MdOutlineMailOutline/>
+                        </Link>
+                        <span> &copy; 2024 Persevi </span>
+                    </footer>
                 </div>
-            </div>
-            <div className="bg-persevi-grey text-white text-center py-4">
-                &copy; 2024 Persevi
             </div>
         </>
     );
