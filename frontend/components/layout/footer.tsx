c67ccfa9fb6ab0e71fcfa94651ace27c4e48ac84
Kelly Kang <flrhyme9@gmail.com>
2024-01-03 18:49:44 +0900

    add: footer, change: navbar

---
+++ frontend/components/layout/footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default function Footer() {
+    return (
+        <>
+            <div className="flex flex-col">
+                <div className="container mx-auto p-4">
+                </div>
+            </div>
+            <div className="bg-persevi-grey text-white text-center py-4">
+                &copy; 2024 Persevi
+            </div>
+        </>
+    );
+}
--- frontend/components/layout/navbar.tsx
+++ frontend/components/layout/navbar.tsx
@@ -10,11 +10,6 @@ export default function Navbar() {
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-persevi-blue dark:border-gray-700">
                         <li>
-                            <Link href="/"
-                               className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                               aria-current="page">Home</Link>
-                        </li>
-                        <li>
                             <Link href="/user/register"
                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">회원가입</Link>
                         </li>
