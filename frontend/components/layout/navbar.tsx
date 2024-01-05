fe93da40ac65b9b37232650bebafc3895b2180ad
Kelly Kang <flrhyme9@gmail.com>
2024-01-05 15:46:55 +0900

    fix: 글씨 색

--- frontend/components/layout/navbar.tsx
+++ frontend/components/layout/navbar.tsx
@@ -18,14 +18,14 @@ export default function Navbar() {
 					<ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 bg-persevi-blue">
 						<li>
 							<Link href="/user/register">
-								<span className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 :text-white md: :hover:text-blue-500  hover:text-white ">
+								<span className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md: :hover:text-blue-500  hover:text-white ">
 									회원가입
 								</span>
 							</Link>
 						</li>
 						<li>
 							<Link href="/user/login">
-								<span className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 :text-white md: :hover:text-blue-500  hover:text-white ">
+								<span className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md: :hover:text-blue-500  hover:text-white ">
 									로그인
 								</span>
 							</Link>
