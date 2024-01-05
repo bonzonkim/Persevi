faf6a31e4023873a21c8f9d82577dc38570a3316
hakhyun98 <xorjsrla123@naver.com>
2024-01-05 16:10:14 +0900

    add: react-calendar 추가

--- frontend/pages/attendcheck/attendcheck.tsx
+++ frontend/pages/attendcheck/attendcheck.tsx
@@ -1,39 +1,31 @@
-// import React, { useState } from 'react';
-// import Calendar from 'react-calendar';
-// import './style.module.css';
-// // import 'react-calendar/dist/Calendar.css';
-// import moment from 'moment';
-//
-//
-// export default function MyApp() {
-//     const [today, onChangeToday] = useState(new Date());
-//
-//     const onChange = () => {
-//         onChangeToday(today);
-//     };
-//
-//     const marks = [
-//         "15-01-2022",
-//         "03-01-2022",
-//         "07-01-2022",
-//         "12-01-2022",
-//         "13-01-2022",
-//         "15-01-2022",
-//     ];
-//
-//     return (
-//         <div>
-//             <Calendar
-//                 className={"react-calendar"}
-//                 onChange={onChange}
-//                 value={today}
-//                 locale="ko-KR"
-//                 tileClassName={({ date, view }) => {
-//                     if (marks.find((x) => x === moment(date).format("DD-MM-YYYY"))) {
-//                         return "highlight";
-//                     }
-//                 }}
-//             />
-//         </div>
-//     );
-// }
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import './style.module.css';
+// import 'react-calendar/dist/Calendar.css';
+import moment from 'moment';
+
+export default function MyApp() {
+	const [today, onChangeToday] = useState(new Date());
+
+	const onChange = () => {
+		onChangeToday(today);
+	};
+
+	const marks = ['15-01-2022', '03-01-2022', '07-01-2022', '12-01-2022', '13-01-2022', '15-01-2022'];
+
+	return (
+		<div>
+			<Calendar
+				className={'react-calendar'}
+				onChange={onChange}
+				value={today}
+				locale="ko-KR"
+				tileClassName={({ date, view }) => {
+					if (marks.find(x => x === moment(date).format('DD-MM-YYYY'))) {
+						return 'highlight';
+					}
+				}}
+			/>
+		</div>
+	);
+}
