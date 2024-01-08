f5bc449c53ec3f6d435a90c528c03a117d5230f5
Kelly Kang <flrhyme9@gmail.com>
2024-01-08 23:34:00 +0900

    fix: 로그인시 navbar에 유저이름 띄우기

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import userRouter from './routes/userRouter';
+import CookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = 3099;
@@ -8,6 +9,14 @@ const PORT = 3099;
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// app.use(
+// 	session({
+// 		secret: 'persevi123',
+// 		resave: false,
+// 		saveUninitialized: true
+// 	})
+// );
+app.use(CookieParser());
 app.use('/api/user', userRouter);
 
 app.get('/hello', (req: Request, res: Response) => {
