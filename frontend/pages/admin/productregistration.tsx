c7f37043670358f3365bfb18a87d880f480793f7
khj980211 <khj980211@naver.com>
2024-01-09 17:10:17 +0900

    modify: color, info 등 여러 input 추가 및 수정

--- frontend/pages/admin/productregistration.tsx
+++ frontend/pages/admin/productregistration.tsx
@@ -1,6 +1,7 @@
 //상품 등록 페이지
 import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
 import axios from 'axios';
+import PhotoAddCard from '@/components/PhotoAddCard';
 
 function productregistration() {
 	const [msg, setMsg] = useState('');
@@ -57,15 +58,15 @@ function productregistration() {
 			<div className="space-y-12">
 				<div className="mt-3 grid grid-cols-1 gap-x-5 gap-y-7 sm:grid-cols-6">
 					<div className="col-span-full">
-						<label htmlFor="productname" className="block text-sm font-medium leading-6 text-white">
+						<label htmlFor="prod_nm" className="block text-sm font-medium leading-6 text-white">
 							상품명
 						</label>
 						<div className="mt-2">
 							<div className="flex rounded-md ring-1 ring-gray-300 sm:max-w-md">
 								<input
 									type="text"
-									name="productname"
-									id="productname"
+									name="prod_nm"
+									id="prod_nm"
 									className="block flex-1 bg-transparent py-1.5 pl-1 placeholder:text-gray-400 sm:text-sm sm:leading-6 text-white"
 									placeholder="상품명"
 								/>
@@ -74,15 +75,15 @@ function productregistration() {
 					</div>
 
 					<div className="col-span-1">
-						<label htmlFor="price" className="block text-sm font-medium text-white">
+						<label htmlFor="prod_price" className="block text-sm font-medium text-white">
 							판매가격(₩)
 						</label>
 						<div className="mt-2">
 							<div className="flex rounded-md ring-1 ring-gray-300 sm:max-w-md">
 								<input
 									type="text"
-									name="price"
-									id="price"
+									name="prod_price"
+									id="prod_price"
 									className="block flex-1 bg-transparent py-1.5 pl-1 placeholder:text-gray-400 sm:text-sm sm:leading-6 text-white"
 									placeholder="판매가격"
 								/>
@@ -91,15 +92,15 @@ function productregistration() {
 					</div>
 
 					<div className="col-span-1">
-						<label htmlFor="price1" className="block text-sm font-medium text-white">
+						<label htmlFor="prod_price1" className="block text-sm font-medium text-white">
 							원가(₩)
 						</label>
 						<div className="mt-2">
 							<div className="flex rounded-md ring-1 ring-gray-300 sm:max-w-md">
 								<input
 									type="text"
-									name="price1"
-									id="price1"
+									name="prod_price1"
+									id="prod_price1"
 									className="block flex-1 bg-transparent py-1.5 pl-1 placeholder:text-gray-400 sm:text-sm sm:leading-6 text-white"
 									placeholder="원가"
 								/>
@@ -108,15 +109,15 @@ function productregistration() {
 					</div>
 
 					<div className="col-span-1">
-						<label htmlFor="price1" className="block text-sm font-medium text-white">
+						<label htmlFor="prod_price2" className="block text-sm font-medium text-white">
 							판매수익(₩)
 						</label>
 						<div className="mt-2">
 							<div className="flex rounded-md ring-1 ring-gray-300 sm:max-w-md">
 								<input
 									type="text"
-									name="price1"
-									id="price1"
+									name="prod_price2"
+									id="prod_price2"
 									className="block flex-1 bg-transparent py-1.5 pl-1 placeholder:text-gray-400 sm:text-sm sm:leading-6 text-white"
 									placeholder="판매수익"
 								/>
@@ -189,7 +190,7 @@ function productregistration() {
 							&nbsp;
 						</label>
 						<div className="mt-2">
-							<div className="flex rounded-md ring-1 ring-gray-300 sm:max-w-md">
+							<div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-0 focus-within:ring-indigo-600 sm:max-w-md">
 								<input
 									type="text"
 									name="size5"
@@ -198,6 +199,66 @@ function productregistration() {
 								/>
 							</div>
 						</div>
+					</div>
+
+					<div className="col-span-full">
+						<label htmlFor="prod_color" className="block text-sm font-medium leading-6 text-white">
+							색상
+						</label>
+						<div className="mt-2">
+							<div className="flex rounded-md ring-1 ring-gray-300 sm:max-w-xl">
+								<input
+									type="text"
+									name="prod_color"
+									id="prod_color"
+									className="block flex-1 bg-transparent py-1.5 pl-1 placeholder:text-gray-400 sm:text-sm sm:leading-6 text-white"
+									placeholder="ex. Black/White/Gray"
+								/>
+							</div>
+						</div>
+					</div>
+
+					<div className="col-span-full">
+						<label htmlFor="prod_info" className="block text-sm font-medium leading-6 text-white">
+							상세 설명
+						</label>
+						<div className="mt-2">
+							<textarea
+								id="prod_info"
+								name="prod_info"
+								rows={10}
+								cols={40}
+								className="block bg-transparent rounded-md text-white p-1 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								defaultValue={''}
+								placeholder="상세 설명"
+							/>
+						</div>
+					</div>
+					<div className="col-start-1 col-span-1">
+						<label htmlFor="prod_imgs" className="block text-sm font-medium leading-6 text-white">
+							제품 사진
+						</label>
+						<div className="mt-2">
+							<div className="mt-2 flex justify-center rounded-lg border border-dashed border-white px-14 py-16">
+								<div className="text-center">
+									<div className="flex text-sm leading-6 text-white text-5xl">
+										<label
+											htmlFor="file-upload"
+											className="relative cursor-pointer rounded-md bg-transparent font-semibold text-white focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-persevi-blue"
+										>
+											<span>+</span>
+											<input id="file-upload" name="file-upload" type="file" className="sr-only" />
+										</label>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-span-1">
+						<label htmlFor="prod_imgs" className="block text-sm font-medium leading-6 text-white">
+							&nbsp;
+						</label>
+						<PhotoAddCard />
 					</div>
 				</div>
 			</div>
