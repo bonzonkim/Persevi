ab03dd1819b0c0a26dc5d0f6890c816e61fc84a3
khj980211 <khj980211@naver.com>
2024-01-15 19:49:16 +0900

    Feat: adminRouter added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import userRouter from './routes/userRouter';
+import adminRouter from './routes/adminRouter';
 
 const app = express();
 const PORT = 3099;
@@ -9,6 +10,7 @@ app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/user', userRouter);
+app.use('/api/admin', adminRouter);
 
 app.get('/hello', (req: Request, res: Response) => {
 	res.json({ msg: 'hello from server' });
