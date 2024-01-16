bca6fd44db277108fd2f345fca89e019f071f692
khj980211 <khj980211@naver.com>
2024-01-16 17:35:44 +0900

    test code delete

--- backend/src/service/productService.ts
+++ backend/src/service/productService.ts
@@ -6,46 +6,7 @@ import { ProductInterface } from '../model/productModel';
 const repository = AppDataSource.getRepository(Product);
 
 export async function productService(productRegisterData: ProductInterface) {
-	try {
-		const {
-			prod_nm,
-			prod_price,
-			prod_price1,
-			prod_price2,
-			prod_size,
-			prod_color,
-			prod_info,
-			prod_img,
-			prod_img1,
-			prod_img2,
-			prod_cate,
-			prod_use
-		} = productRegisterData;
-
-		const product = repository.create({
-			prod_nm,
-			prod_price,
-			prod_price1,
-			prod_price2,
-			prod_size,
-			prod_color,
-			prod_info,
-			prod_img,
-			prod_img1,
-			prod_img2,
-			prod_cate,
-			prod_use
-		});
-
-		const prodData = await repository.save(product);
-
-		return prodData;
-	} catch (e) {
-		console.log(`error: ${e}`);
-		throw new Error('상품등록 실패');
-	}
 	// try {
-	// 	// eslint-disable-next-line prefer-const
 	// 	const {
 	// 		prod_nm,
 	// 		prod_price,
@@ -60,7 +21,6 @@ export async function productService(productRegisterData: ProductInterface) {
 	// 		prod_cate,
 	// 		prod_use
 	// 	} = productRegisterData;
-
 	// 	const product = repository.create({
 	// 		prod_nm,
 	// 		prod_price,
@@ -75,40 +35,10 @@ export async function productService(productRegisterData: ProductInterface) {
 	// 		prod_cate,
 	// 		prod_use
 	// 	});
-	// 	const productData = await repository.save(product);
-
-	// 	return productData;
+	// 	const prodData = await repository.save(product);
+	// 	return prodData;
 	// } catch (e) {
-	// 	console.log(e);
+	// 	console.log(`error: ${e}`);
 	// 	throw new Error('상품등록 실패');
 	// }
 }
-// export async function loginService(userLoginData: UserInterface) {
-// 	try {
-// 		const { uid, pwd } = userLoginData;
-//
-// 		const loginUser = await repository.findOneBy({ uid: uid });
-// 		const loginObj = {
-// 			loginUser: loginUser,
-// 			msg: ''
-// 		};
-//
-// 		if (!loginUser) {
-// 			loginObj.msg = '사용자를 찾을 수 없습니다.';
-// 			throw new Error('사용자를 찾을 수 없습니다.');
-// 		}
-//
-// 		const isPwdMatch = await bcrypt.compare(pwd, loginUser.pwd);
-//
-// 		console.log(`isPwdMatch ${isPwdMatch}`);
-// 		if (!isPwdMatch) {
-// 			loginObj.msg = '비밀번호가 일치하지 않습니다.';
-// 			throw new Error('비밀번호가 일치하지 않습니다.');
-// 		}
-//
-// 		return loginObj;
-// 	} catch (e) {
-// 		console.log(e);
-// 		throw new Error('로그인 실패');
-// 	}
-// }
