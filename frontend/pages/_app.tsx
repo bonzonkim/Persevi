310517405ce9ebc73126e96d9dbd27f7d4f4af1c
khj980211 <khj980211@naver.com>
2024-01-16 17:32:56 +0900

    ?

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,21 +1,20 @@
 import '../styles/globals.css';
-import Navbar from "@/components/layout/navbar";
-import Footer from "@/components/layout/footer";
+import Navbar from '@/components/layout/navbar';
+import Footer from '@/components/layout/footer';
 import Layout from '@/components/layout/layout';
-
 
 /*
   _app은 로직, 전역 스타일 등 컴포넌트에 공통 데이터를 다룬다.
   /styles/globals.css 가 글로벌 css파일
  */
 const App = ({ Component, pageProps }: any) => {
-  return(
-      <div>
-      <Layout>
-          <Component {...pageProps}/>
-      </Layout>
-      </div>
-  )
+	return (
+		<div>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</div>
+	);
 };
 
 export default App;
