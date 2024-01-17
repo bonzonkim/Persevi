cca21d521aae17a33dcfef10a4a537fe6243c4f2
khj980211 <khj980211@naver.com>
2024-01-18 00:27:14 +0900

    Convert size array to string

--- backend/src/controller/adminController.ts
+++ backend/src/controller/adminController.ts
@@ -4,9 +4,9 @@ import { productService } from '../service/productService';
 
 export async function productregistration(req: Request<object, object, ProductInterface>, res: Response) {
 	try {
+		req.body.prod_size = req.body.prod_size.toString();
 		console.log(req.body);
 		const prodData = await productService(req.body);
-		console.log(prodData);
 		res.json({ msg: `상품등록이 완료되었습니다.` });
 	} catch (e) {
 		console.log(`error: ${e}`);
