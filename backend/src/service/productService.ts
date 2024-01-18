8ade57f6dee2ac40519e8bd0f775e3ab6a7e8bc3
khj980211 <khj980211@naver.com>
2024-01-18 16:36:43 +0900

    image column 주석처리

--- backend/src/service/productService.ts
+++ backend/src/service/productService.ts
@@ -1,4 +1,4 @@
-import { Collection } from 'typeorm';
+//import { Collection } from 'typeorm';
 import { AppDataSource } from '../config/db';
 import { Product } from '../entity/Product';
 import { ProductInterface } from '../model/productModel';
@@ -6,39 +6,37 @@ import { ProductInterface } from '../model/productModel';
 const repository = AppDataSource.getRepository(Product);
 
 export async function productService(productRegisterData: ProductInterface) {
-	// try {
-	// 	const {
-	// 		prod_nm,
-	// 		prod_price,
-	// 		prod_price1,
-	// 		prod_price2,
-	// 		prod_size,
-	// 		prod_color,
-	// 		prod_info,
-	// 		prod_img,
-	// 		prod_img1,
-	// 		prod_img2,
-	// 		prod_cate,
-	// 		prod_use
-	// 	} = productRegisterData;
-	// 	const product = repository.create({
-	// 		prod_nm,
-	// 		prod_price,
-	// 		prod_price1,
-	// 		prod_price2,
-	// 		prod_size,
-	// 		prod_color,
-	// 		prod_info,
-	// 		prod_img,
-	// 		prod_img1,
-	// 		prod_img2,
-	// 		prod_cate,
-	// 		prod_use
-	// 	});
-	// 	const prodData = await repository.save(product);
-	// 	return prodData;
-	// } catch (e) {
-	// 	console.log(`error: ${e}`);
-	// 	throw new Error('상품등록 실패');
-	// }
+	try {
+		const {
+			prod_nm,
+			prod_price,
+			prod_price1,
+			prod_price2,
+			prod_size,
+			prod_color,
+			prod_info,
+			// prod_img,
+			// prod_img1,
+			// prod_img2,
+			prod_cate,
+			prod_use
+		} = productRegisterData;
+
+		const product = repository.create({
+			prod_nm,
+			prod_price,
+			prod_price1,
+			prod_price2,
+			prod_size,
+			prod_color,
+			prod_info,
+			prod_cate,
+			prod_use
+		});
+		const prodData = await repository.save(product);
+		return prodData;
+	} catch (e) {
+		console.log(`error: ${e}`);
+		throw new Error('상품등록 실패');
+	}
 }
